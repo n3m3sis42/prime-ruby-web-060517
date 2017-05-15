746a6bbce02f2a01aacc1455dc4924a7e8e24022
Sarah Evans <sarah.g.evans42@gmail.com>
2017-05-15 21:38:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  return false if number <= 1
+  return true if number <= 3
+
+  factors = Array(2...number)
+  factors.each do |factor|
+    if number % factor == 0
+      return false
+    end
+  end
+  true
+end  # Add  code here!
